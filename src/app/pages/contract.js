34bf380b54471b33d18a33f372e17185495e543d
EdgynCode <nguyenhuutan080603@gmail.com>
2024-01-08 19:56:47 +0700

    navbar complete

--- src/app/pages/contract.js
+++ src/app/pages/contract.js
@@ -1,14 +1,14 @@
 import Footer from "../components/footer";
 import Navbar from "../components/navbar";
 
-const HomePage = () => {
+const Contract = () => {
     return (
         <div>
             <Navbar />
-            <h1>Welcome to the Home Page!</h1>
+            <h1>Welcome to the Contract Page!</h1>
             <Footer/>
         </div>
     );
 }
 
-export default HomePage;
+export default Contract;
