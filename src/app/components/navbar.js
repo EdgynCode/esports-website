12c84ddc9ae7730cd896ba2fde0907aae2dcb254
EdgynCode <nguyenhuutan080603@gmail.com>
2024-01-20 20:38:09 +0700

    add search bar to navbar

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React from 'react';
 import { NavLink } from "react-router-dom";
 import "./navbar.css";
 
 const Navbar = () => {
   return (
     <nav className="topnav">
-      <NavLink to="/home" activeClassName="active">Home</NavLink>
-      <NavLink to="/schedule" activeClassName="active">Schedule</NavLink>
-      <NavLink to="/contract" activeClassName="active">Global Contract Database</NavLink>
+      <NavLink to="/home" activeclassname="active">Home</NavLink>
+      <NavLink to="/schedule" activeclassname="active">Schedule</NavLink>
+      <NavLink to="/contract" activeclassname="active">Global Contract Database</NavLink>
+      <form action="#" className="form-container">
+        <div className="topnav-form">
+            <input type="email" className="form-control" placeholder="Search..."/>
+            <button type="submit" className="search-button">
+              <i>Search</i>
+            </button>
+        </div>
+      </form>
     </nav>
   );
 };
