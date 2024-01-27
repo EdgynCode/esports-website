39a6ccd5395d392c06941adaf793e3513b11cca7
EdgynCode <nguyenhuutan080603@gmail.com>
2024-01-27 13:15:13 +0700

    add table layout

--- src/app/pages/contract.js
+++ src/app/pages/contract.js
@@ -5,10 +5,26 @@ const Contract = () => {
     return (
         <div>
             <Navbar />
-            <h1>Welcome to the Contract Page!</h1>
+            <table style={{borderCollapse: 'collapse', width: '100%'}}>
+                <tr>
+                    <th style={{border: '1px solid #ddd', padding: '8px'}}>League</th>
+                    <th style={{border: '1px solid #ddd', padding: '8px'}}>Team</th>
+                    <th style={{border: '1px solid #ddd', padding: '8px'}}>Official Summoner Name</th>
+                    <th style={{border: '1px solid #ddd', padding: '8px'}}>Position</th>
+                    <th style={{border: '1px solid #ddd', padding: '8px'}}>Legal Family Name</th>
+                    <th style={{border: '1px solid #ddd', padding: '8px'}}>Legal First Name</th>
+                    <th style={{border: '1px solid #ddd', padding: '8px'}}>End Date (Month, Day, Year)</th>
+                    <th style={{border: '1px solid #ddd', padding: '8px'}}>Residency</th>
+                    <th style={{border: '1px solid #ddd', padding: '8px'}}>Status</th>
+                    <th style={{border: '1px solid #ddd', padding: '8px'}}>Tricode</th>
+                    <th style={{border: '1px solid #ddd', padding: '8px'}}>Team Contact</th>
+                </tr>
+                
+            </table>
             <Footer/>
         </div>
     );
 }
+
 
 export default Contract;
