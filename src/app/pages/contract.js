10c8045e2fc09630e6b3b77e752fb8a328e647c7
EdgynCode <117133793+EdgynCode@users.noreply.github.com>
2024-02-10 14:33:05 +0700

    Update contract.js

--- src/app/pages/contract.js
+++ src/app/pages/contract.js
@@ -26,21 +26,42 @@ const Contract = () => {
             </div>
             
             <table className="table-style">
-                <tr>
-                    <th className="table-header-style">League</th>
-                    <th className="table-header-style">Team</th>
-                    <th className="table-header-style">Official Summoner Name</th>
-                    <th className="table-header-style">Position</th>
-                    <th className="table-header-style">Legal Family Name</th>
-                    <th className="table-header-style">Legal First Name</th>
-                    <th className="table-header-style">End Date (Month, Day, Year)</th>
-                    <th className="table-header-style">Residency</th>
-                    <th className="table-header-style">Status</th>
-                    <th className="table-header-style">Tricode</th>
-                    <th className="table-header-style">Team Contact</th>
-                </tr>
-                
+                <thead>
+                    <tr>
+                        <th className="table-header-style">League</th>
+                        <th className="table-header-style">Team</th>
+                        <th className="table-header-style">Official Summoner Name</th>
+                        <th className="table-header-style">Position</th>
+                        <th className="table-header-style">Legal Family Name</th>
+                        <th className="table-header-style">Legal First Name</th>
+                        <th className="table-header-style">Nationality</th>
+                        <th className="table-header-style">End Date (Month, Day, Year)</th>
+                        <th className="table-header-style">Residency</th>
+                        <th className="table-header-style">Status</th>
+                        <th className="table-header-style">Tricode</th>
+                        <th className="table-header-style">Team Contact</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((row, index) => (
+                        <tr key={index}>
+                            <td>{row.league}</td>
+                            <td>{row.team}</td>
+                            <td>{row.INGAMENAME}</td>
+                            <td>{row.POSITION}</td>
+                            <td>{row.NAME}</td>
+                            <td>{row.FIRSTNAME}</td>
+                            <td>{row.nationality}</td>
+                            <td>{row.CONTRACTENDDATE}</td>
+                            <td>{row.RESIDENCY}</td>
+                            <td>{row.STATUS}</td>
+                            <td>{row.tricode}</td>
+                            <td>{row.TEAMCONTACT}</td>
+                        </tr>
+                    ))}
+                </tbody>
             </table>
+
             <Footer/>
         </div>
     );
