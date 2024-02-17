351d0978661b31fe947a292bdb07a1865d208df8
EdgynCode <nguyenhuutan080603@gmail.com>
2024-02-17 23:45:57 +0700

    modify server

--- src/api/server.js
+++ src/api/server.js
@@ -1,9 +1,5 @@
 require('dotenv').config();
-const express = require('express');
 const { Pool } = require('pg');
-const cors = require('cors');
-
-const app = express();
 
 // Configure PostgreSQL connection
 const pool = new Pool({
@@ -13,12 +9,10 @@ const pool = new Pool({
   }
 });
 
-// Use CORS middleware
-app.use(cors({
-  origin: process.env.CORS_ORIGIN
-}));
+module.exports = async (req, res) => {
+  // Enable CORS
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
-app.get('/api/data', async (req, res) => {
   try {
     const client = await pool.connect();
     const result = await client.query('SELECT "LEAGUES"."LEAGUEID" AS League, "TEAMS"."TEAMNAME" AS Team, "MEMBERS"."INGAMENAME" AS SummonerName, "MEMBERS"."POSITION" AS Position, "MEMBERS"."NAME" AS Name, "MEMBERS"."FIRSTNAME" AS FirstName, "NATIONS"."NATIONCODE" AS Nationality, "MEMBERS"."CONTRACTENDDATE" AS EndDate, "MEMBERS"."RESIDENCY" AS Residency, "MEMBERS"."STATUS" AS Status, "TEAMS"."TEAMCODE" AS Tricode, "TEAMS"."TEAMCONTACT" AS TeamContact FROM "LEAGUES" JOIN "PARTICIPATE" ON "LEAGUES"."LEAGUEID" = "PARTICIPATE"."LEAGUEID" JOIN "TEAMS" ON "PARTICIPATE"."TEAMID" = "TEAMS"."TEAMID" JOIN "PLAY" ON "TEAMS"."TEAMID" = "PLAY"."TEAMID" JOIN "MEMBERS" ON "PLAY"."PLAYERID" = "MEMBERS"."PLAYERID" JOIN "HAS_NATIONALITY" ON "MEMBERS"."PLAYERID" = "HAS_NATIONALITY"."PLAYERID" JOIN "NATIONS" ON "HAS_NATIONALITY"."NATIONCODE" = "NATIONS"."NATIONCODE";');
@@ -29,9 +23,4 @@ app.get('/api/data', async (req, res) => {
     console.error(err);
     res.status(500).json({ error: 'Internal Server Error' });
   }
-});
-
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+};
