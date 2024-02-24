ae583f5e4c1bb167df96ea68969118b4f7538047
EdgynCode <nguyenhuutan080603@gmail.com>
2024-02-24 13:18:02 +0700

    modify server

--- src/app/pages/contract.js
+++ src/app/pages/contract.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Footer from "../components/footer";
 import Navbar from "../components/navbar";
 import './contract.css'
