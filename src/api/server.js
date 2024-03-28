defee105941e1c8f92257840260b23ad1577a04e
Nguyen Huu Tan <117133793+EdgynCode@users.noreply.github.com>
2024-03-28 23:57:55 +0700

    Update server.js

--- src/api/server.js
+++ src/api/server.js
@@ -8,12 +8,8 @@ const port = process.env.PORT || 5000;
 
 // Configure PostgreSQL connection
 const pool = new Pool({
-  user: process.env.POSTGRES_USER,
-  host: process.env.POSTGRES_HOST,
-  database: process.env.POSTGRES_DATABASE,
-  password: process.env.POSTGRES_PASSWORD,
-  connectionString: process.env.POSTGRES_PRISMA_URL
-});
+  connectionString: process.env.POSTGRES_URL ,
+})
 
 // Use CORS middleware
 app.use(cors({
