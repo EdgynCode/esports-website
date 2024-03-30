f638084bfa5e31510d861bf7d4897a59a0eb5ff0
Nguyen Huu Tan <117133793+EdgynCode@users.noreply.github.com>
2024-03-30 12:25:18 +0700

    Update navbar.js

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -8,14 +8,14 @@ const Navbar = () => {
       <NavLink to="/home" activeclassname="active">Home</NavLink>
       <NavLink to="/schedule" activeclassname="active">Schedule</NavLink>
       <NavLink to="/contract" activeclassname="active">Global Contract Database</NavLink>
-      <form action="#" className="form-container">
-        // <div className="topnav-form">
-        //     <input type="email" className="form-control" placeholder="Search..."/>
-        //     <button type="submit" className="search-button">
-        //       <i>Search</i>
-        //     </button>
-        // </div>
-      </form>
+      // <form action="#" className="form-container">
+      //   <div className="topnav-form">
+      //       <input type="email" className="form-control" placeholder="Search..."/>
+      //       <button type="submit" className="search-button">
+      //         <i>Search</i>
+      //       </button>
+      //   </div>
+      // </form>
     </nav>
   );
 };
