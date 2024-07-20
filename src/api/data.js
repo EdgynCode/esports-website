517fafe7a4375fae06f3e6f755ad8e167e3cf880
Nguyen Huu Tan <117133793+EdgynCode@users.noreply.github.com>
2024-07-20 23:22:05 +0700

    Update data.js

--- src/api/data.js
+++ src/api/data.js
@@ -1,17 +1,21 @@
-import { createPool } from '@vercel/postgres';
+import { Pool } from 'pg';
 
-const pool = createPool({
+const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
 });
 
 export async function fetchLeagueData() {
+  const client = await pool.connect();
   try {
-    const data = await pool.sql`SELECT * FROM "CONTRACT"`;
+    const res = await client.query('SELECT * FROM "CONTRACT"');
     console.log('Data fetch completed.');
-    return data.rows;
+    return res.rows;
   }
   catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch league data.');
+  }
+  finally {
+    client.release();
   }
 }
